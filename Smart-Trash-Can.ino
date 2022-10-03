d1d28498ade9a263c0beaf7663938880fc780d7b
italobap <48602485+italobap@users.noreply.github.com>
2022-10-03 14:28:28 -0300

    Notificação esp8266 v2
    Tentativa de usar o push safer

--- Smart-Trash-Can.ino
+++ Smart-Trash-Can.ino
@@ -1,32 +1,51 @@
 //Trabalho de Gustavo, Italo e Lucas
 
-#include <Wire.h>
+//Instalação de Esp8266
+//https://www.robocore.net/tutoriais/como-programar-nodemcu-arduino-ide#:~:text=Configurando%20a%20Arduino%20IDE&text=Agora%20clique%20em%20Ferramentas%20%2D%3E%20Placa,placas%20da%20sua%20Arduino%20IDE.
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiClientSecure.h>
-#include <Servo.h>
+#include <Servo.h>4
+#include <Wire.h>
+
+//Push safer = Notificação via push safer
+// https://androidgeek.pt/enviar-e-receber-notificacoes-push-em-tempo-real-no-android-ou-ios
+#include <Pushsafer.h>
+#include <ArduinoJson.h>
+#define PushsaferKey "HxqogWuaFBM6vx8LfEqW"
+
+/*WiFiClientSecure client;*/
+WiFiClient client;
+Pushsafer pushsafer(PushsaferKey, client);
 
 
+//Configuração do servo
 Servo servo;
+int servoPin = 13;
+
 //Configuração do ultrassônico
 int trigPin = 14;    
 int echoPin = 12;   
-int servoPin = 13;
+
 long duracao, distancia;   
 long distmed[3];   //vetor de distancia media
 
+
 // ============= Você precisa preencher com seus dados ===================================
-const char* ssid = "VIVOFIBRA-1911";
-const char* password = "essaehruim";
- 
-const char* PushBulletAPIKEY = "v9663055D5F9F8BA"; //A sua chave de API. Veja na sua conta Pushbullet.
+const char* ssid = "Redmi Note 7";
+const char* password = "12345678";
+
+//Pushbulet = Notificação via push bullet
+//https://capsistema.com.br/index.php/2020/10/27/notificacao-pushbullet-de-internet-das-coisas-pushingbox-e-arduino/#Implementar_a_notificacao_usando_ESP8266_ou_Arduino
+//https://www.automalabs.com.br/esp8266-como-enviar-notificacoes-usando-o-servico-pushbullet/
+/*const char* PushBulletAPIKEY = "v9663055D5F9F8BA"; //A sua chave de API. Veja na sua conta Pushbullet.
 const String message_title = "Enviado do ESP8266";
 const String message_body = "Funciona!";
 const char* host = "api.pushbullet.com";
 
 // Pushingbox API
 char *api_server = "api.pushingbox.com";
-char *deviceId = "v9663055D5F9F8BA";  //Usar o seu codigo depois.
+char *deviceId = "v9663055D5F9F8BA";  //Usar o seu codigo depois.*/
 
 void setup() {       
     Serial.begin(9600);
@@ -59,27 +78,13 @@ void medicao() {
   //O 29,1 é a velocidade do som (que é 343,5 m/s => 1 / 0,03435 = 29,1). Portanto, o resultado está em cm, não em polegadas
 }
 
-//Manda notificação
-void sendNotification() 
+//Manda notificação via push bullet
+/*void sendNotification() 
 {
-  WiFiClient client; 
-  Serial.println("Enviando notificação para " + String(api_server));
-  if (client.connect(api_server, 80)) { 
-     Serial.println("Conectado ao servidor");
-     String message = "devid=" + String(deviceId) + "Teste" +"\r\n\r\n";
-     
-     client.print("POST /pushingbox HTTP/1.1\n");
-     client.print("Host: api.pushingbox.com\n");
-     client.print("Connection: close\n");
-     client.print("Content-Type: application/x-www-form-urlencoded\n");
-     client.print("Content-Length: ");
-     client.print(message.length());
-     client.print("\n\n");
-     client.print(message);
-  }
-  client.stop();
-  Serial.println("Notificação enviada!");
-}
+  struct PushSaferInput input;
+  input.message = "This is a test message";
+  input.title = "Hello!";
+}*/
 
 void loop() {
   //Distancia media
@@ -93,14 +98,18 @@ void loop() {
   
   //Abre tampa lixeira
   if ( distancia<30 ) {
+    struct PushSaferInput input;
+    input.message = "This is a test message";
+    input.title = "Hello!";
+    
     servo.attach(servoPin); //Anexa o servo a um pino
     delay(1);
     servo.write(0);  
     delay(3000);       
     servo.write(150); //Grava um valor no servo controlando o seu eixo  
     delay(3000);
-    servo.detach(); // Desconecte o servo do seu pino
-    sendNotification();    
+    Serial.println(pushsafer.sendEvent(input));
+    Serial.println("Sent");
   }
   Serial.print(distancia);
 }
